81163a2f12c081f3010edd836c9a6f4d50c6535f
c_orungpro <promise.orung@ng.ab-inbev.com>
2025-12-06 04:06:43 +0100

    Update text content

--- intro.js
+++ intro.js
@@ -1,3 +1,3 @@
-console.log('Promise Orung')
+console.log('My name is Promise Orung')
 console.log('I am 24 years old')
-console.log('Javascript')
+console.log('My favourite programming language is Javascript')
